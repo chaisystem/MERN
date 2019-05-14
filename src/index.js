09f8bcf8cfb0e1422bab82a29667027fc6e629af
chaisystem <isoto@netwarmonitor.com>
2019-05-14 12:47:45 -0500

    ++
    nodemon actualiza el servidor cada vez que guardamos

--- src/index.js
+++ src/index.js
@@ -13,6 +13,6 @@ const app = express(); // lo guardo en un objeto
 
 
 // Start servidor
-app.listen(3000, () => {
-    console.log('server on port 3000');
+app.listen(4000, () => {
+    console.log('server on port 4000');
 });
