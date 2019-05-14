43b540c7dcf6b74fff0abb4761964976f776aa7f
chaisystem <isoto@netwarmonitor.com>
2019-05-14 12:40:19 -0500

    Creación de script en package json
    npm start

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const express = require('express'); // obtengo express
 const app = express(); // lo guardo en un objeto
 
+
+// setings
+
+// Middlewares
+
+
+// Routes
+
+// Static files
+
+
+// Start servidor
 app.listen(3000, () => {
     console.log('server on port 3000');
 });
