a98d4ae08fe9310d5763c9c95bc991584311f123
chaisystem <isoto@netwarmonitor.com>
2019-05-15 14:33:53 -0500

    babel
    npm install babel-core babel-loader babel-preset-react babel-preset-env
    -D
    
    Instamos modulo de babel que son los encargados de traducir JSX y js
    moderno

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,16 @@
 // alert('alert desde indexjs , aqui a react');
+
+import React, { Component } from 'react'; // 
+import { render } from 'react-dom';
+
+// SINTAXIS JSX
+class App extends Comment{
+    render(){
+        return (
+            <h1>chaisystem</h1>
+        )
+    }
+}
+// SINTAXIS JSX FIN
+
+render(<App/>, document.getElementById('app'))
--- src/public/bundle.js
+++ src/public/bundle.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("alert('alert desde indexjs que sera trasnfomado');\n\n//# sourceURL=webpack:///./src/app/index.js?");
+eval("throw new Error(\"Module parse failed: Unexpected token (9:12)\\nYou may need an appropriate loader to handle this file type.\\n|     render(){\\n|         return (\\n>             <h1>chaisystem</h1>\\n|         )\\n|     }\");\n\n//# sourceURL=webpack:///./src/app/index.js?");
 
 /***/ })
 
