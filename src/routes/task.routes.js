a5b79d8829f4c673aa7cd1a3f02404b0356dade2
chaisystem <isoto@netwarmonitor.com>
2019-05-14 19:58:59 -0500

    Update PUT
    POSTMAN

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -18,13 +18,20 @@ router.get('/', async (req, res) =>{
     res.json(tasks);
 });
 
- router.post('/', async (req, res) =>{
+ router.post('/', async (req, res) =>{ // async esperando un await
     const { title, description} = req.body;
     const task = new Task({title,description});
     await task.save(); // awiat nos sirve para que se ejecute la instuccion y despues continue con la siguiente linea, en este caso guardar la tarea
     res.json({ status: 'task saved'});
 });
 
+router.put('/:id', async (req, res) =>{
+    const {title, description} = req.body;
+    const newTask = {title,description};
+    await Task.findByIdAndUpdate(req.params.id, newTask); // obtenemos el id y segundo paramentro los datos ... await esperamos que termine
+    console.log(req.params.id);
+    res.json('task updated');
+});
 
 
 module.exports = router;
