444bfa30c2129844197f6816c0e6e9669125577b
chaisystem <isoto@netwarmonitor.com>
2019-05-15 19:29:54 -0500

    H1 en react
    Correcion de módulos nom

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
-class App extends Comment{
-    render(){
-        return (
-            <h1>chaisystem</h1>
-        )
-    }
-}
+import App from './App';
 
 render(<App/>, document.getElementById('app'));
